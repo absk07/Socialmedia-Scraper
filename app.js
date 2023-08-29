949e48ed3f769dd9a6e319f46cc70ffe0936740e
absk07 <noobmaster26128@gmail.com>
2023-08-29 23:29:24 +0530

    modified cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', 'https://app-dev.onwintop.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
